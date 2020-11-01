b3acb259db294765958bcd2da30b9b2bcf73eb20
jraices <jraices@ist.ac.at>
2020-11-01 15:47:34 +0100

    new examples

--- genestructure.R
+++ genestructure.R
@@ -2,46 +2,116 @@ library(genemodel)
 
 genemodel.plot(model=AT5G62640, start=25149433, bpstop=25152541, orientation="reverse", xaxis=T)
 
-tabela <- read.csv("IntronExonTable.output", sep="\t", header=T)
-tabela
-AT5G62640
+tabela <- read.csv("~/dmel_analysis/gonads/raws/tempF1.txt", sep="\t", header=T)
+head(tabela)
+head(AT5G62640)
 
 EI = strsplit(tabela$ExonIntronSeq[1], ",")
 SE = strsplit(tabela$Starts.Ends[1], ",")
 primeiro = data.frame(unlist(EI), unlist(SE))
 
-colnames(primeiro) <- c("type", "coordinates")
+colnames(primeiro) <- c("factor", "ranges")
 
 for(i in 1:length(primeiro$type)){
-	if(grepl("_", primeiro$type[i])){
-		primeiro$type[i] = "intron"
-	} else{
+	if(grepl("co", primeiro$type[i])){
 		primeiro$type[i] = "exon"
+	} else{
+		primeiro$type[i] = "intron"
 	}
 }
+
+GRanges(primeiro)
+primeiro[7,1] = "coding_region"
+primeiro[7,2] = "7076710-7092667"
+primeiro[5,1] = "exon"
+primeiro[6,1] = "intron"
+primeiro[6,2] = "7082026-7084598"
+genemodel.plot(model=primeiro, start=7066700, bpstop=7100000, orientation="reverse", xaxis=T)
+
+library(ggbio)
+library(biovizBase)
+
+ggplot() + geom_alignment(primeiro, type = "model")
+autoplot(primeiro, aes(type = model))
+
+library(GenomicRanges)
+grfr <- GRanges(seqnames = sample(c("chr1", "chr2", "chr3"), size = N, replace = TRUE), IRanges(start = sample(1:300, size = N, replace = TRUE), width = sample(70:75, size = N,replace = TRUE)), strand = sample(c("+", "-"), size = N, replace = TRUE), value = rnorm(N, 10, 3), score = rnorm(N, 100, 30), sample = sample(c("Normal", "Tumor"), size = N, replace = TRUE), pair = sample(letters, size = N, replace = TRUE))
+auplot(primeiro, aes(color = strand, fill = strand))
+
+autoplot(object, ..., xlab, ylab, main, which)
+autoplot(object, ..., xlab, ylab, main,
+        geom = c("tile", "raster"), axis.text.angle = NULL,
+        hjust = 0.5, na.value = NULL,
+        rownames.label = TRUE, colnames.label = TRUE,
+        axis.text.x = TRUE, axis.text.y = TRUE)
+
+
+source("https://bioconductor.org/biocLite.R") 
+biocLite("Gviz")
+
+
+plot.new()
+axis(1, seq(0, 1, 0.2))
+
+GeneStructure <- function(x){
+	for(i in 1:length(x)){
+		x$rang[i] <- strsplit(x$Starts.Ends[i], ",")[[1]]
+		for(j in 1:length(x$rang[i])){
+			XX[j] <- strsplit(x$rang[i][j], "-")[[1]]
+			a = 0.5 + i
+			b = 0.5 + i*2
+			ggplot(XX, aes(XX[1],XX[2],a,b,"cornflowerblue")) + geom_rect()
+		}
+	}
+}
+
+ggplot() + geom_rect(tabela, aes)
+tabela$cores <- "cornflowerblue"
+tabela
+GeneStructure(tabela)
 primeiro
-primeiro$coordinates[2]="14173923-14174099"
-primeiro$coordinates[4]="14174270-14174461"
-primeiro$coordinates[3]="14174100-14174572"
-primeiro$coordinates[1]="14173749-14174572"
-primeiro$coordinates[5]="14174462-14174572"
-primeiro$coordinates[6]="14176406-14176542"
-primeiro$coordinates[7]="14176612..14176691"
-primeiro$coordinates[8]="14177175..14177477"
-primeiro$coordinates[9]="2R:14177825..14178021"
-primeiro$coordinates[10]="2R:14177840..14178021"
-primeiro$coordinates[11]="2R:14177861..14178021"
-primeiro$coordinates[12]="2R:14178098..14178200"
-primeiro$coordinates[13]="2R:14178098..14178203"
-primeiro$coordinates[14]=""
-primeiro$coordinates[15]=""
-primeiro$coordinates[16]=""
-primeiro$coordinates[17]=""
-primeiro$coordinates[18]=""
-primeiro$coordinates[19]=""
-primeiro$coordinates[20]=""
+
+library(plotly)
+
+library(data.table)
+df<-data.table(Product=letters[1:10], minX=1:10, maxX=5:14, minY= 10:1, maxY=14:5)
+NewTable <- data.frame(Read=character(), XX1=integer(), XX2=integer(), YY1=integer(), YY2=integer())
+for(i in length(tabela)){
+	partOne <- strsplit(tabela$Starts.Ends[i], ",")[[1]]
+	for(j in length(partOne)){
+		partThree <- strsplit(partOne[j], "-")
+		NewTable$Read[nrow(NewTable)+1] <- tabela$transcript[i]
+		NewTable$XX1[nrow(NewTable)+1] <- partThree[[1]][1]
+		NewTable$XX2[nrow(NewTable)+1] <- partThree[[1]][2]
+		NewTable$YY1[nrow(NewTable)+1] <- c(0.5+i)
+		NewTable$YY2[nrow(NewTable)+1] <- c(0.5+2*i)
+	}
+}
+a <- strsplit(partOne[1], "-")[[1]]
+a
+partOne[2]
+partThree
+NewTable
+df.t<-data.table(rbind( df[,list(Product,X=minX,Y=minY)],
+        df[,list(Product,X=minX,Y=maxY)],
+        df[,list(Product,X=maxX,Y=minY)],
+        df[,list(Product,X=maxX,Y=maxY)]))[order(Product,X,Y)]
+p <- ggplot(df,aes(xmin=minX,xmax=maxX,ymin=minY,ymax=maxY,fill=Product))+ geom_rect()
+fig <- ggplotly(p)
+fig
 
 
-primeiro = primeiro[-c(2,3,4,5,10,11,12,13,18,19),]
-genemodel.plot(model=primeiro, start=0, bpstop=3100, orientation="reverse", xaxis=T)
+
+plot.new() 
+axis(2, seq(7076710, 7092667, 1000))
+ggplot(aes()) + geom_rect(aes(x=c(10,325,10,325), y=c(1,1,1.5,1.5), fill="cornflowerblue"))
+
+grid(led = 2)
+
+
+plot(runif(10), runif(10), 
+          xlim=c(0, 1), ylim=c(0,1), 
+	       axes=FALSE, #Don't plot the axis 
+	       type="n",  #hide the points
+	            ylab="", xlab="") #No axis labels""""""')))))
 
--- table_creator.py
+++ table_creator.py
@@ -1,118 +1,112 @@
 #!bin/usr/python3
 # import packages we will need to work with dataframes and all
-import sys, argparse, os
-from Bio import SeqIO
-import numpy as np
-import pandas as pd
-pd.options.mode.chained_assignment = None  # default='warn'
-from datetime import datetime
+import sys, argparse, os # import packages to call command line commands, to parse arguments given when calling the program
+from Bio import SeqIO # import part of the Bio package that deals with input and output of sequences/fasta/fastq/etc
+import numpy as np # imports numpy for dealing with numbers and stats
+import pandas as pd # pandas is for dealing with dataframes
+pd.options.mode.chained_assignment = None  # default='warn' # changes the options for pandas so it doesn't give warnings for everything. in other words: I'm bad and don't want to know about it =\
+from datetime import datetime # allows us to add the date/time to the code/output
 
 # Read arguments (file names)
-parser = argparse.ArgumentParser(description='Program to create a table with the list of introns/exons that comprise different reads.')
-parser.add_argument("--blat", required=True, type=str, help='Output from blat of reads against introns and exons')
-parser.add_argument("--output", default="IntronExonTable.output", help="Desired name for the output file. Default is IntronExonTable.output", type=str)
-parser.add_argument("--log", default="TableMaker.log", help="Desired name for the log file. Default is TableMaker.log", type=str)
+# allows to create an automatic message for usage and checks if all the parameters have been provided, or give the default input for each parameter that has a default value/file
+parser = argparse.ArgumentParser(description='Program to create a table with the list of introns/exons that comprise different reads.') # sets the description for the full program, in case it's called with no arguments
+parser.add_argument("--blat", required=True, type=str, help='Output from blat of reads against introns and exons') # states a required argument, a blat file, and has a helper message in case you don't provide it, or ask for help
+parser.add_argument("--fasta", default="/nfs/scistore03/vicosgrp/jraices/melanogaster/dmel_exon_intron.fasta", type=str, help='Fasta file used in blat. Default is the file with exons and introns in Drosophila melanogaster') # sets the default fasta file to be used in case it is not provided by the user
+parser.add_argument("--output", default="IntronExonTable.output", help="Desired name for the output file. Default is IntronExonTable.output", type=str) # sets the output file name if none is given. If there's already a file with that name, it will be overwritten.
+parser.add_argument("--log", default="TableMaker.log", help="Desired name for the log file. Default is TableMaker.log", type=str) # sets the log file name if none is given. The new log will be appended to the end of the given file.
 
-args = parser.parse_args()
+args = parser.parse_args() # stores arguments given in args, so we can reference them.
 
-try:
+try: # makes sure you can open all the files
     log = open(args.log, "a") # "a" will append to the end of the file
     # open input
     inputz = open(args.blat, "r") # opens file as read only
+    fastaz = open(args.fasta, "r") # opens file as read only
     outputz = open(args.output, "w") # "w" will overwrite file
 # If files do not open:
-except FileNotFoundError or IOError:
+except FileNotFoundError or IOError: # if any of the files is not openned, shuts down the program and logs the errors
     # report to log
-    log.write("One or more of the files given was not available.\n")
-    print("File name is not valid. Please try again.")
+    log.write("One or more of the files given was not available.\n") # prints error to log
+    print("File name is not valid. Please try again.") # prints error to standard output
     # exit program with erros
-    sys.exit("Files are not valid.")
+    sys.exit("Files are not valid.") # prints error as system error and leaves the program
 
-inputz.close() # close input to prevent naming errors
+outputz.write("Transcript\tGene\tExonIntronSeq\tUniqID\tTranscriptSize\tQueryStarts-Ends\tDBStarts-Ends\tFlags\tSequence\n")# open output and print header
 
-# open output and print header
-outputz.write("Transcript\tGene\tExonIntronSeq\tStarts-Ends\tFlags\n")
+log.write("\n\n#########################\nNew usage of table_creator.py at " + str(datetime.now()) + ".\n\t The input used was " + str(inputz.name) + ", the output was: " + str(outputz.name) + ", and the fasta file used was:" + str(fastaz.name) + ".\n") # open log and print timestamp, inputs names, and output name.
 
-# open log and print timestamp, input name, and output name.
-log.write("\n\n#########################\nNew usage of table_creator.py at " + str(datetime.now()) + ".\n\t The input used was " + str(inputz.name) + ", the output was: " + str(outputz.name) + ", and the log file is " + str(log.name) + ".\n")
-EIfasta={}
-for seq_record in SeqIO.parse("/nfs/scistore03/vicosgrp/jraices/melanogaster/dmel_exon_intron.fasta", "fasta"):
-        acessorary=seq_record.description.split("; ")
-        location = [s for s in acessorary if "loc" in s]
-        EIfasta[seq_record.id]=str(location).split("=")[1]
-# read input into a dataframe:
-inputz = pd.read_csv(args.blat, sep='\t', header=None)
+seq_records = SeqIO.to_dict(SeqIO.parse(fastaz, "fasta")) #open fasta file and store its info
+
+inputz = pd.read_csv(args.blat, sep='\t', header=None)# read input into a dataframe:
 # give columns names to make life easier
 #columns=['0.match', '1.mismatch', '2.RepMatch', '3.Ns', '4.QGapCount', '5.QGapBases', '6.TGapCount', '7.TGapBases', '8.strand', '9.Qname[exon/intron]', '10.Qsize', '11.Qstart', '12.Qend', '13.Tname[read]', '14.Tsize', '15.Tstart', '16.Tend', '17.BlockCount', '18.BlockSizes', '19.qStarts', '20.tStarts', '21.Gene', '22.ExonIntronSeq', '23.Flags'])
-inputz.columns = ['match', 'mismatch', 'RepMatch', 'Ns', 'QGapCount', 'QGapBases', 'TGapCount', 'TGapBases', 'strand', 'Qname', 'Qsize', 'Qstart', 'Qend', 'Tname', 'Tsize', 'Tstart', 'Tend', 'BlockCount', 'BlockSizes', 'qStarts', 'tStarts']# to add: '21.Gene', '22.ExonIntronSeq', '23.Flags'
+inputz.columns = ['match', 'mismatch', 'RepMatch', 'Ns', 'QGapCount', 'QGapBases', 'TGapCount', 'TGapBases', 'strand', 'Qname', 'Qsize', 'Qstart', 'Qend', 'Tname', 'Tsize', 'Tstart', 'Tend', 'BlockCount', 'BlockSizes', 'qStarts', 'tStarts'] # sets the names of the columns from the blat file
+# to add: '21.Gene', '22.ExonIntronSeq', '23.Flags', '24.QueryStarts-Ends', '25.DBStarts-Ends', '26.Sequence'
 # add columns we will fill out later with a standard value (in this case, NA = Not Available)
-inputz['Gene']="NA"
-inputz['ExonIntronSeq']="NA"
-inputz['Flags']="NA"
-inputz['Starts-Ends']="NA"
+inputz['Gene']="NA" # create a new column, and sets all values to NA
+inputz['ExonIntronSeq']="NA" # create a new column, and sets all values to NA
+inputz['Size']="NA" # create a new column, and sets all values to NA
+inputz['Flags']="NA" # create a new column, and sets all values to NA
+inputz['QueryStarts-Ends']="NA" # create a new column, and sets all values to NA
+inputz['DBStarts-Ends']="NA" # create a new column, and sets all values to NA
+inputz['Sequence']="NA" #create a new column, and sets all values to NA
 
-# get uniq transcripts from input[:,13] -> i.e. 14th column
-UniqTranscripts = inputz['Tname'].unique()
+UniqTranscripts = inputz['Tname'].unique() # get uniq transcripts from input[:,13] -> i.e. 14th column
 # makes a new data frame to put what will be outputed
-#FinalPrint = pd.DataFrame(columns=['Tname', 'Gene', 'ExonIntronSeq', 'Starts-Ends', 'Flags'])
-FinalPrint = pd.DataFrame(columns = ['match', 'mismatch', 'RepMatch', 'Ns', 'QGapCount', 'QGapBases', 'TGapCount', 'TGapBases', 'strand', 'Qname', 'Qsize', 'Qstart', 'Qend', 'Tname', 'Tsize', 'Tstart', 'Tend', 'BlockCount', 'BlockSizes', 'qStarts', 'tStarts', 'Gene', 'ExonIntronSeq', 'Starts-Ends', 'Flags']) # initiates temporary data frame for this transcript
-# for each uniq transcript:
-for i in UniqTranscripts:
+FinalPrint = pd.DataFrame(columns = ['Tname', 'Gene', 'ExonIntronSeq', 'UniqID', 'Size', 'QStarts-Ends', 'DBStars-Ends', 'Flags', 'Sequence']) # initiates data frame for transcripts
+
+for transcript_name in UniqTranscripts: # for each uniq transcript:
+    EIdata={} # initiate an empty hash to store things in
     flag = "" # initiate an empty flag item
-    NewHash = pd.DataFrame(columns = ['match', 'mismatch', 'RepMatch', 'Ns', 'QGapCount', 'QGapBases', 'TGapCount', 'TGapBases', 'strand', 'Qname', 'Qsize', 'Qstart', 'Qend', 'Tname', 'Tsize', 'Tstart', 'Tend', 'BlockCount', 'BlockSizes', 'qStarts', 'tStarts', 'Gene', 'ExonIntronSeq', 'Starts-Ends', 'Flags']) # initiates temporary data frame for this transcript
+    TranscriptData = pd.DataFrame(columns = ['match', 'mismatch', 'RepMatch', 'Ns', 'QGapCount', 'QGapBases', 'TGapCount', 'TGapBases', 'strand', 'Qname', 'Qsize', 'Qstart', 'Qend', 'Tname', 'Tsize', 'Tstart', 'Tend', 'BlockCount', 'BlockSizes', 'qStarts', 'tStarts', 'Gene', 'ExonIntronSeq', 'Starts-Ends', 'Flags']) # initiates temporary data frame for transcript
     # get all apearances of it in input
-    temp = inputz[inputz['Tname'] == i] # makes a temporary variable only with the one read
-    temp.sort_values(['Tstart','Tend'], inplace=True)
-    # for each instace:
-    for j in range(len(temp)):
-        #get ext gene from other blat to check positions
-        # check if it's an intron:
-        if "intron" in temp['Qname'].iloc[j]:
+    InputSubset = inputz[inputz['Tname'] == transcript_name] # makes a temporary variable only with the one read/transcript
+    InputSubset.sort_values(['Tstart','Tend'], inplace=True) # sort according to Tstart first, and within that, accoridng to Tend
+    for transcript_instance in range(len(InputSubset)): # for each instace of that transcript
+        if "intron" in InputSubset['Qname'].iloc[transcript_instance]: # check if it's an intron:
             # if read has intron, make an error alert
-            log.write("\t Error: INTRON alert. The "+temp['Tname'].iloc[j]+"has the following match, "+temp['Qname'].iloc[j]+", that is an intron.\n")
-            # divide intron name, so we can see the genes and introns involoved
-            Part1, Part2, NewItemB = temp['Qname'].iloc[j].split(":")
-            NewItemA, Gene2 = Part2.split("_")
-            NewItem = NewItemA+"_"+NewItemB
-            MehIntron, Gene1 = Part1.split("_")
-            if Gene1 != Gene2:
-                # if any of the named genes is different from each other
-                log.write("\t\t Error: Intron between different genes in "+temp['Qname'].iloc[j]+" for read"+temp['Tname'].iloc[j]+"!!!\n")
-                NewGenes=Gene1+"_"+Gene2
-                if flag == "": # add flag
-                    flag = "z"
-                elif not ("z" in flag):
-                    flag = flag+",z"
-            else: # add flag
-                NewGenes=Gene1
-                if flag == "":
-                    flag = "w"
-                elif not "w" in flag:
-                    flag = flag+",w"
+            log.write("\t Error: INTRON alert. The "+InputSubset['Tname'].iloc[transcript_instance]+" has the following match, "+InputSubset['Qname'].iloc[transcript_instance]+", that is an intron.\n") # writes to the log that there was an intron (form: intron_FBgn#:#_FBgn#:#)
+            Intron_FBgn1, IntronStart_FBgn2, IntronEnd = InputSubset['Qname'].iloc[transcript_instance].split(":") # divide intron name, so we can see the genes and introns involoved
+            IntronStart, Gene2 = IntronStart_FBgn2.split("_") # subdivides the intron's name again to get the actual gene name
+            EIitem = IntronStart+"_"+IntronEnd # sets the exons between which the exon is
+            WordIntron, Gene1 = Intron_FBgn1.split("_") # gets the second gene's id
+            if Gene1 != Gene2: # if any of the named genes is different from each other, that is bad and we address it
+                log.write("\t\t Error: Intron between different genes in "+InputSubset['Qname'].iloc[transcript_instance]+" for read"+InputSubset['Tname'].iloc[transcript_instance]+"!!!\n") # write the genes and trancript with the intron between genes <o>
+                NewGene=Gene1+"_"+Gene2 # sets the name of the gene as the names of both genes that comprise it.
+                if flag == "": # add flag to empty flag variable
+                    flag = "z" # add the flag that means intron between different genes
+                elif not ("z" in flag): # if the flag variable is not empty and doesn't have a "z" flag
+                    flag = flag+",z" # adds "z" to the flag variable
+            else: # i.e. if the genes that comprise the exon are the same
+                NewGene=Gene1 #sets the new gene to be added
+                if flag == "": # adds flag for intron if the flag variable is empty
+                    flag = "w" # "w" is the flag for "normal" introns""
+                elif not "w" in flag: # if the flag variable is not empty, but doesn't already have a "w"
+                    flag = flag+",w" # adds "w" to flags
         # parse name/genes
         else: # if read has no intron ^^
-            NewGenes, NewItem = temp['Qname'].iloc[j].split(":")
-        if NewHash.empty: # if the new hash is empty we add things the easiest way ;)
-            NewHash = NewHash.append(temp.iloc[j])
-            NewHash['Gene'].iloc[0] = NewGenes
-            NewHash['ExonIntronSeq'].iloc[0] = NewItem
-        elif not NewGenes in NewHash['Gene'].iloc[0]: # if the new hash is not empty and the gene we found is not in the new hash gene name
-            if "_" in NewGenes:
-                NewGenes2 = NewGenes.split("_")
-                for k in range(len(NewGenes2)):
-                    if not NewGenes2[k] in NewHash['Gene'].iloc[0]:
-                        NewHash['Gene'].iloc[0] = NewHash['Gene'].iloc[0]+"_"+NewGenes2[k]
-            else:
-                NewHash['Gene'].iloc[0] = NewHash['Gene'].iloc[0]+"_"+NewGenes
-            if flag == "":
-                flag = "y"
-            elif not "y" in flag:
-                flag = flag+",y"
+            NewGene, EIitem = InputSubset['Qname'].iloc[transcript_instance].split(":") # gets the name of the gene and exon that comprises that bit
+        if TranscriptData.empty: # if the new hash is empty we add things the easiest way ;)
+            TranscriptData = TranscriptData.append(InputSubset.iloc[transcript_instance]) # adds this instance to the transcripts database
+            TranscriptData['Gene'].iloc[0] = NewGene # adds new gene name to the transcripts hash
+            TranscriptData['ExonIntronSeq'].iloc[0] = EIitem # adds intron/exon item to transcripts hash
+        elif not NewGene in TranscriptData['Gene'].iloc[0]: # if the new hash is not empty and the gene we found is not in the new hash gene name
+            if "_" in NewGene: # if more than one gen comprises the already known genes in the transcript (their names are separated with _ )
+                NewGenes2 = NewGenes.split("_") # split where there's a _
+                for k in range(len(NewGenes2)): # gfor each gene splited above
+                    if not NewGenes2[k] in NewHash['Gene'].iloc[0]: # if the splited gene is not in the stored ones
+                        NewHash['Gene'].iloc[0] = NewHash['Gene'].iloc[0]+"_"+NewGenes2[k] # adds splited gene to stored ones
+            else: # if the stored gene is just one gene (doesn't have a _)
+                NewHash['Gene'].iloc[0] = NewHash['Gene'].iloc[0]+"_"+NewGenes # just adds the new gene to the stored ones
+            if flag == "": # if the flag variable is empty
+                flag = "y" # add "y" to the flags
+            elif not "y" in flag: # if flag variable is not empty but doesn't have the "y" flag
+                flag = flag+",y" # adds the "y" flag to it
             #NewHash['ExonIntronSeq'].iloc[0] = NewHash['ExonIntronSeq'].iloc[0]+","+NewItem
         #else:
             #NewHash['ExonIntronSeq'].iloc[0] = NewHash['ExonIntronSeq'].iloc[0]+","+NewItem
         # if Qstart > Qend
-        if (temp['Tstart'].iloc[j] > temp['Tend'].iloc[j]):
+        if (temp['Tstart'].iloc[j] > temp['Tend'].iloc[j]): #
             log.write("\t Error: "+temp['Qname'].iloc[j]+" had start bigger than end in the "+temp['Tname'].iloc[j]+".\n")
             if flag == "":
                 flag = "a"
@@ -203,27 +197,30 @@ for i in UniqTranscripts:
                 eita = 1+13
             else:
                 print("Something has gone terribly wrong!!! <o>"+str(Arya['start'].iloc[m])+" "+str(Arya['end'].iloc[m])+"\t"+str(Sansa['start'].iloc[o])+" "+str(Sansa['end'].iloc[o])+"\n")
-    print(sansadrop)
-    print(Sansa)
     if sansadrop!=[]:
         Sansa = Sansa.drop(Sansa.index[sansadrop])
-    print(Sansa)
     StarkSE = pd.DataFrame(columns=['SE'])
+    StarkI = pd.DataFrame(columns=['I'])
     for q in range(len(Sansa)):
         NewSE = str(Sansa['start'].iloc[q])+"-"+str(Sansa['end'].iloc[q])
-        StarkSE = StarkSE.append([NewSE])
-    NewHash['Starts-Ends'] = ",".join(str(StarkSE))
-    NewHash['ExonIntronSeq'] = ",".join(str(Sansa['item']))
-    NewHash['Flags'].iloc[0]=flag
+        if q == 0:
+            StarkSE = str(NewSE)
+            StarkI = str(Sansa['item'].iloc[q])
+        else:
+            StarkSE = str(StarkSE)+","+str(NewSE)
+            StarkI = str(StarkI)+","+str(Sansa['item'].iloc[q])
+    NewHash['Starts-Ends'] = StarkSE
+    NewHash['ExonIntronSeq'] = StarkI
+    NewHash['Flags'].iloc[0]=flag # add flags to hash
     # make PrintingHash NewItem with Qname, Tname, and flags
-    FinalPrint = FinalPrint.append(NewHash, ignore_index=True)
+    FinalPrint = FinalPrint.append([NewHash], ignore_index=True) # add new item to end of hash
+
 #FinalPrint.to_string(outputz, header=True, index=False)
-for i in range(len(FinalPrint)):
-    outputz.write(str(FinalPrint['Tname'].iloc[i])+"\t"+str(FinalPrint['Gene'].iloc[i])+"\t"+str(FinalPrint['ExonIntronSeq'].iloc[i])+"\t"+str(FinalPrint['Starts-Ends'].iloc[i])+"\t"+str(FinalPrint['Flags'].iloc[i])+"\n")
-# close output
-outputz.close()
+for i in range(len(FinalPrint)): # for each item in the FinalPrint hash
+    outputz.write(str(FinalPrint['Tname'].iloc[i])+"\t"+str(FinalPrint['Gene'].iloc[i])+"\t"+str(FinalPrint['ExonIntronSeq'].iloc[i])+"\t"+str(FinalPrint['Starts-Ends'].iloc[i])+"\t"+str(FinalPrint['Flags'].iloc[i])+"\n") # print database to output
+
+outputz.close()# close output
 # print number of errors, number of reads, number of flags reported, and flags itself to log
-log.write("\n\n")
-# close log
-log.close()
+log.write("\n\n") # creates space between 
+log.close() # closes log
 # end
